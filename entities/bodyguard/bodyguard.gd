81f5e470ddd42440c2afdf7c628623d56d77258a
khaizter <vashh.dev@gmail.com>
2023-05-27 20:20:20 +0800

    Added bodyguard

---
+++ entities/bodyguard/bodyguard.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+export(Array,String,FILE, "*.json") var dialogue_files
+export(String) var npc_name
+
+onready var dialogue = $dialogue
+onready var interact_button = $control/interact_button
+
+var interacting = false
+var player = null
+var player_in_range = false
+
+var working_elevator = false
+
+func _ready():
+	interact_button.visible = false
+	dialogue.initialize_dialogues(dialogue_files)
+
+func _process(delta):
+	interact_button.visible = player_in_range
+
+func interaction():
+	if (not interacting):
+		interacting = true
+		player.set_physics_process(false)
+		if not working_elevator:
+			dialogue.start(0)
+		else:
+			dialogue.start(1)
+
+func _on_bodyguard_body_entered(body):
+	if body.is_in_group("player"):
+		player = body
+		player_in_range = true
+		body.connect("player_interact",self,"interaction")
+
+
+func _on_bodyguard_body_exited(body):
+	if body.is_in_group("player"):
+		player = null
+		player_in_range = false
+		body.disconnect("player_interact",self,"interaction")
+
+func _on_dialogue_finish_dialogue(page):
+	interacting = false
+	player.set_physics_process(true)
--- levels/floor_3/floor_3.gd
+++ levels/floor_3/floor_3.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var door = $door
+onready var bodyguard = $bodyguard
 
 var task_1 = false
 var task_2 = false
@@ -8,6 +9,7 @@ var task_3 = false
 
 func _process(delta):
 	if task_1 and task_2 and task_3:
+		bodyguard.working_elevator = true
 		door.open = true
 
 func _on_computer_quest_done():
