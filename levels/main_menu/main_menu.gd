f7731f5d8b3ba925c1b26624b86e781efe360c20
khaizter <vashh.dev@gmail.com>
2023-06-23 02:39:15 +0800

    level menu

--- levels/main_menu/main_menu.gd
+++ levels/main_menu/main_menu.gd
@@ -1,15 +1,60 @@
 extends CanvasLayer
 
-export var level:PackedScene
+export var level_1_scene:PackedScene
+export var level_2_scene:PackedScene
+export var level_3_scene:PackedScene
+export var level_4_scene:PackedScene
+export var level_5_scene:PackedScene
+
+
+onready var main = $control/main
+onready var stages = $control/stages
+onready var level_1 = $"control/stages/level_1"
+onready var level_2 = $"control/stages/level_2"
+onready var level_3 = $"control/stages/level_3"
+onready var level_4 = $"control/stages/level_4"
+onready var level_5 = $"control/stages/level_5"
 
 func _ready():
-	pass
+	level_1.disabled = not Player.is_level_unlocked(1)
+	level_2.disabled = not Player.is_level_unlocked(2)
+	level_3.disabled = not Player.is_level_unlocked(3)
+	level_4.disabled = not Player.is_level_unlocked(4)
+	level_5.disabled = not Player.is_level_unlocked(5)
+	main.visible = true
+	stages.visible = false
 
 
 func _on_start_button_pressed():
-	if (level):
-		get_tree().change_scene_to(level)
-
+	main.visible = false
+	stages.visible = true
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+func _on_back_pressed():
+	main.visible = true
+	stages.visible = false
+
+func _on_level_1_pressed():
+	if (level_1_scene):
+		get_tree().change_scene_to(level_1_scene)
+
+func _on_level_2_pressed():
+	if (level_2_scene):
+		get_tree().change_scene_to(level_2_scene)
+
+func _on_level_3_pressed():
+	if (level_3_scene):
+		get_tree().change_scene_to(level_3_scene)
+
+func _on_level_4_pressed():
+	if (level_4_scene):
+		get_tree().change_scene_to(level_4_scene)
+
+func _on_level_5_pressed():
+	if (level_5_scene):
+		get_tree().change_scene_to(level_5_scene)
+
+
+
--- singleton/Player.gd
+++ singleton/Player.gd
@@ -2,26 +2,21 @@ extends Node
 
 signal player_move(direction, times)
 
-var data_name = "undefined"
+var data_name = "Player"
+
+var levels = {1 : true, 2 : true, 3  : true, 4 : true, 5 : false}
 
 func _ready():
 	pass
-
-func moveRight(times = 1):
-	emit_signal("player_move", Vector2.RIGHT, times)
-
-func moveLeft(times = 1):
-	emit_signal("player_move", Vector2.LEFT, times)
-
-func moveUp(times = 1):
-	emit_signal("player_move", Vector2.UP, times)
-	
-func moveDown(times = 1):
-	emit_signal("player_move", Vector2.DOWN, times)
-
 
 func set_name(value):
 	data_name = value
 
 func get_name():
 	return data_name
+	
+func unlock_level(value):
+	levels[value] = true
+
+func is_level_unlocked(value):
+	return levels[value]
