18d57be7950845e189357e8915f11597b7f2fe92
khaizter <vashh.dev@gmail.com>
2023-07-10 13:47:57 +0800

    dictionary fix

--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 import sys
 def eval():
 	try:
-		ldict = {}
-		exec('''test = "print"
-print(test)''',globals(),ldict)
-		return ["default_output", True]
+		ldict= {"bookdict": {
+		  "pages": 69,
+		  "quality": "mint",
+		  "price": 420
+		}}
+		exec('''print(bookdict["quality"])''',globals(),ldict)
+		return ['Wrong value', False]
 	except Exception as error:
 		return [type(error).__name__ + '-' + str(error),False]
 res = eval()
